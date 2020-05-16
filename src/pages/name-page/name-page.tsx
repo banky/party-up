8dc9ad5f1568ba339f02ca91ea86db993a38b884
Banky <bankiadebajo1@gmail.com>
2020-05-15 23:50:35 -0400

    Add react router and some simple pages (#1)

---
+++ src/pages/name-page/name-page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NamePage = () => {
+  return <h2>Name Page</h2>;
+};
---
+++ src/pages/room-page/room-page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RoomPage = () => {
+  return <h2>Room Page</h2>;
+};
