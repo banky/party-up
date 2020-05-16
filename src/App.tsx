5e47d17bc7c916bb7a7273820ba6b68c2de715c6
bankya <bankiadebajo@hotmail.com>
2020-05-15 23:48:16 -0400

    Add react router and some simple pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LandingPage } from "./pages/landing-page/landing-page";
+import { NamePage } from "./pages/name-page/name-page";
+import { RoomPage } from "./pages/room-page/room-page";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path="/landing">
+          <LandingPage />
+        </Route>
+        <Route path="/name">
+          <NamePage />
+        </Route>
+        <Route path="/">
+          <RoomPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
