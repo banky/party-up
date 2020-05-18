2c36ccc8a3b1e3c3e0f01321179e785de022a6c1
Banky <bankiadebajo1@gmail.com>
2020-05-18 17:21:29 -0400

    Add firebase (#4)
    * Initial firebase setup
    
    * Add working firebase :)

---
+++ src/lib/firebase/firebase.ts
@@ -0,0 +1,27 @@
+import app from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+class Firebase {
+  database: app.database.Database;
+
+  constructor() {
+    if (!app.apps.length) {
+      app.initializeApp(firebaseConfig);
+    }
+
+    this.database = app.database();
+  }
+}
+
+export default Firebase;
