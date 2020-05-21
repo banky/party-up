772b73ad8d09072e0eb023912f2b8ba5ed33b8da
bankya <bankiadebajo@hotmail.com>
2020-05-20 21:58:43 -0400

    Refactor spotify auth to make it cute

--- src/lib/spotify/helpers.ts
+++ src/lib/spotify/helpers.ts
@@ -44,7 +44,15 @@ export const loadScript = (attributes: ScriptAttributes): Promise<any> => {
 
 declare var Spotify: any;
 
-export const initializePlayer = (authToken: string) => {
+declare global {
+  interface Window {
+    setSpotifyAuthToken: any;
+    spotifyPlayer: any;
+    onSpotifyWebPlaybackSDKReady: any;
+  }
+}
+
+export const initializePlayer = async (authToken: string) => {
   const player = new Spotify.Player({
     name: "Web Playback SDK Quick Start Player",
     getOAuthToken: (cb: Function) => {
@@ -80,4 +88,64 @@ export const getPlayerOptions = () => {
     playerId,
     authToken,
   };
+};
+
+export const openSpotifyLoginWindow = () => {
+  const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const response_type = "token";
+  const redirect_uri = "http:%2F%2Flocalhost:3000%2Fspotify-callback";
+  const scopes = "streaming user-modify-playback-state ";
+
+  const spotify_auth_url =
+    "https://accounts.spotify.com/authorize" +
+    "?client_id=" +
+    client_id +
+    "&response_type=" +
+    response_type +
+    "&redirect_uri=" +
+    redirect_uri +
+    "&scope=" +
+    encodeURIComponent(scopes);
+
+  return window.open(
+    spotify_auth_url,
+    "_blank",
+    "location=yes,height=600,width=600,scrollbars=yes,status=yes"
+  );
+};
+
+export const getAuthTokenFromChildWindow = async (
+  childWindow: Window | null
+) => {
+  let authToken = "";
+
+  // Set a function that can be called by the child window
+  window.setSpotifyAuthToken = (data: string) => {
+    authToken = data;
+  };
+
+  // Check if the child window has been closed every so often and resolve when it closes
+  await new Promise((resolve) => {
+    const interval = setInterval(() => {
+      if (childWindow?.closed) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 500);
+  });
+
+  return authToken;
+};
+
+export const loadSpotifyWebPlayer = () => {
+  return new Promise((resolve) => {
+    loadScript({
+      defer: true,
+      id: "spotify-player",
+      source: "https://sdk.scdn.co/spotify-player.js",
+    });
+
+    // This promise resolves when the spotify web player has been loaded
+    window.onSpotifyWebPlaybackSDKReady = resolve;
+  });
 };
--- src/lib/spotify/index.ts
+++ src/lib/spotify/index.ts
@@ -1,65 +1,24 @@
 import { SearchType } from "../constants";
-import { loadScript, getPlayerOptions, initializePlayer } from "./helpers";
+import {
+  getPlayerOptions,
+  initializePlayer,
+  openSpotifyLoginWindow,
+  getAuthTokenFromChildWindow,
+  loadSpotifyWebPlayer,
+} from "./helpers";
 
-declare global {
-  interface Window {
-    setSpotifyAuthToken: any;
-    spotifyPlayer: any;
-    onSpotifyWebPlaybackSDKReady: any;
-  }
-}
-
-export const configure = async () => {
-  window.onSpotifyWebPlaybackSDKReady = initializePlayer;
-
-  // We need onSpotifyWebPlaybackSDKReady to exist before loading the sdk. This makes me sad
-  await loadScript({
-    defer: true,
-    id: "spotify-player",
-    source: "https://sdk.scdn.co/spotify-player.js",
-  });
-};
+export const configure = () => {};
 
 export const authorize = async (): Promise<string> => {
-  const client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-  const response_type = "token";
-  const redirect_uri = "http:%2F%2Flocalhost:3000%2Fspotify-callback";
-  const scopes = "streaming user-modify-playback-state ";
+  const childWindow = openSpotifyLoginWindow();
 
-  const spotify_auth_url =
-    "https://accounts.spotify.com/authorize" +
-    "?client_id=" +
-    client_id +
-    "&response_type=" +
-    response_type +
-    "&redirect_uri=" +
-    redirect_uri +
-    "&scope=" +
-    encodeURIComponent(scopes);
+  const authToken = await getAuthTokenFromChildWindow(childWindow);
 
-  const childWindow = window.open(
-    spotify_auth_url,
-    "_blank",
-    "location=yes,height=600,width=600,scrollbars=yes,status=yes"
-  );
+  await loadSpotifyWebPlayer();
 
-  let authToken = "";
+  initializePlayer(authToken);
 
-  // Set a function that can be called by the child window
-  window.setSpotifyAuthToken = (data: string) => {
-    authToken = data;
-  };
-
-  // Check if the child window has been closed every so often and resolve when it closes
-  return await new Promise((resolution) => {
-    const interval = setInterval(() => {
-      if (childWindow?.closed) {
-        clearInterval(interval);
-        initializePlayer(authToken);
-        resolution(authToken);
-      }
-    }, 500);
-  });
+  return Promise.resolve(authToken);
 };
 
 export const unauthorize = (): void => {};
