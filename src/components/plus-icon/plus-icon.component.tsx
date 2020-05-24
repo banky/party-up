287191a52f1602763419bb1804fb132e312a7768
bankya <bankiadebajo1@gmail.com>
2020-05-24 16:30:47 -0400

    Extract queue to component. Add queue remove

--- src/components/plus-icon/plus-icon.component.tsx
+++ src/components/plus-icon/plus-icon.component.tsx
@@ -18,8 +18,8 @@ export const PlusIcon = ({ className, onClick }: PlusIconProps) => (
     <path
       d="M19.5 3V16.5M19.5 16.5L19.5 33M19.5 16.5H3M19.5 16.5H33"
       stroke="#EE6352"
-      stroke-width="6"
-      stroke-linecap="round"
+      strokeWidth="6"
+      strokeLinecap="round"
     />
   </svg>
 );
