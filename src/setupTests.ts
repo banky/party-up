1bd8d145fb4daee0e42350faeac00448b150fca1
bankya <bankiadebajo1@gmail.com>
2020-05-26 19:47:23 -0400

    Add env variables to setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+process.env = Object.assign(process.env, {
+  REACT_APP_SPOTIFY_CLIENT_ID: "fake-spotify-client-id",
+  REACT_APP_BASE_URL: "http://fake-base-url",
+});
