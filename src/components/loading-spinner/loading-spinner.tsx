5e9fcd29f120447f3fb73b943052904ac2456217
banky <bankiadebajo1@gmail.com>
2020-09-27 19:35:18 -0400

    Add loading indicator on entire app

---
+++ src/components/loading-spinner/loading-spinner.tsx
@@ -0,0 +1,28 @@
+import styled, { keyframes } from "styled-components";
+
+const rotate = keyframes`
+  0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+`;
+
+export const LoadingSpinner = styled.div`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+
+  &:after {
+    content: " ";
+    display: block;
+    width: 40px;
+    height: 40px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #484d6d;
+    border-color: #484d6d transparent #484d6d transparent;
+    animation: ${rotate} 1.2s linear infinite;
+  }
+`;
