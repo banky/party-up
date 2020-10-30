82885f6732d317d07f3a9aeddcb7a1a8a6cb27b4
Banky Adebajo <bankiadebajo1@gmail.com>
2020-10-29 20:28:20 -0400

    Revert useDebouncedCallback (#262)
    * Revert useDebouncedCallback to get it back working
    
    * Fix sorting issue on test
    
    * Consume the new useDebouncedCallback

--- src/hooks/use-debounced-callback.ts
+++ src/hooks/use-debounced-callback.ts
@@ -3,21 +3,27 @@ import { useRef, useCallback } from "react";
 /**
  * Call a function after it hasn't been called for the wait period
  * @param func A function to be called
+ * @param dependencies Dependencies for func
  * @param wait Wait period after function hasn't been called for
  * @returns A memoized function that is debounced
  */
-export const useDebouncedCallback = (func: VoidFunction, wait: number) => {
+export const useDebouncedCallback = (
+  func: VoidFunction,
+  dependencies: any[],
+  wait: number
+) => {
   const timeout = useRef<number>();
+  const memoizedFunc = useCallback(func, [...dependencies]);
 
   return useCallback(() => {
     const later = () => {
       clearTimeout(timeout.current);
-      func();
+      memoizedFunc();
     };
 
     clearTimeout(timeout.current);
     timeout.current = setTimeout(later, wait);
 
     return () => clearTimeout(timeout.current);
-  }, [func, wait]);
+  }, [memoizedFunc, wait]);
 };
